a4ff9c39cb3e5d30077d1194d96e145c055cc0f4
Keion Yan <jay.walk3r911@gmail.com>
2025-11-17 19:09:10 +0000

    smub

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -4,21 +4,66 @@
 # Topics Covered:
 # Creating lists, indexing, slicing, appending, popping, sorting, reversing.
 
+#collections are used to store multiple items in a single variable
+#lists are ordered collections of items
+#lists are mutable, meaning you can change their content
+#lists are created using square brackets []
+
+list_of_fruits = ["apple", "banana","cherry","date"]
+print(list_of_fruits) 
+print (type(list_of_fruits))
+print(list_of_fruits[0])
+print(list_of_fruits[1])
+print(list_of_fruits[-1])
+print(list_of_fruits[1:3])
+list_of_fruits.reverse()
+print(list_of_fruits)
+print(list_of_fruits[::-1])
+list_of_fruits.append("elderberry") #add stuff to the end
+list_of_fruits.append("mango")
+list_of_fruits.append("pineapple")
+print(list_of_fruits)
+list_of_fruits.extend (["fig","grape","honeydew"])
+print(list_of_fruits)
+list_of_fruits.reverse()
+print(list_of_fruits)
+popped_item = list_of_fruits.pop() #pop removes the last one
+print(popped_item)
+print(list_of_fruits)
+list_of_fruits.insert(1, "blueberry")
+print(list_of_fruits)
+#removing a specific item by value
+list_of_fruits.remove("banana")
+print(list_of_fruits)
+list_of_fruits.insert(3,"smub")
+print(list_of_fruits)
+list_of_fruits.sort() #sorts the list in ascending order
+print(list_of_fruits)
+
+#why use lists instead of individual variables
+#imagien u have 100 iteems to manage
+list_of_items = list(range(1,1001)) # creates a list of numbers
+print(list_of_items)
+print(len(list_of_items)) # 1000
+popped_item = list_of_items.pop()
+print(list_of_items)
+list_of_items.extend (range(1001,2001))
+print(list_of_items)
 # Examples:
 
-my_list = ['apple', 'banana', 'cherry']
-print(my_list[0])         # apple
-print(my_list[1:])        # ['banana', 'cherry']
+# my_list = ['apple', 'banana', 'cherry']
+# print(my_list[0])         # apple
+# print(my_list[1:])        # ['banana', 'cherry']
 
-my_list.append('grape')
-print(my_list)
+# my_list.append('grape')
+# print(my_list)
 
-my_list.pop(1)
-print(my_list)
+# my_list.pop(1)
+# print(my_list)
 
-numbers = [3, 1, 4, 2]
-numbers.sort()
-print(numbers)
+# numbers = [3, 1, 4, 2]
+# numbers.sort()
+# print(numbers)
 
 
 # Practice Problems:
